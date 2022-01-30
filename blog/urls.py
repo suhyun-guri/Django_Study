7a44ef56547893540bff44a6cc991038c0bd0459
suhyun <myksh0903@naver.com>
2022-01-30 13:46:31 +0900

    CBV로 blog post 상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('<int:pk>/', views.single_post_page),
+    # path('<int:pk>/', views.single_post_page), #FBV 형식일 때 
+    path('<int:pk>/', views.PostDetail.as_view()),
     # path('', views.index), #FBV 형식일 때 
     path('', views.PostList.as_view())
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class PostList(ListView):
     model = Post    
     
-    
+class PostDetail(DetailView):
+    model = Post
