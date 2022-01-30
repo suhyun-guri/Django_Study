767deec98d0e467f99e59e6967c32bed7df83b4f
suhyun <myksh0903@naver.com>
2022-01-30 13:40:20 +0900

    CBV로 blog post 목록 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),
+    # path('<int:pk>/', views.single_post_page),
+    # path('', views.index), #FBV 형식일 때 
+    path('', views.PostList.as_view())
 ]
 
